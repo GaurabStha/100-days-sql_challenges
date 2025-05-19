56faf2df2c59ba422f737c949929591c4b2ce148
Gaurab Shrestha <gaurabian111@gmail.com>
2025-05-19 03:09:47 +0100

    File Updated

--- SQL-Challenge-day-2.sql
+++ SQL-Challenge-day-2.sql
@@ -3,8 +3,7 @@
 	-- percentage change in revenue. The output should include the year-month date (YYYY-MM) 
 	-- and percentage change, rounded to the 2nd decimal point, and sorted from the beginning
 	-- of the year to the end of the year. The percentage change column will be populated from
-	-- the 2nd month forward and calculated as 
-	-- ((this month’s revenue — last month’s revenue) / last month’s revenue)*100.
+	-- the 2nd month forward and calculated as ((this month’s revenue — last month’s revenue) / last month’s revenue)*100.
 
 show databases;
 

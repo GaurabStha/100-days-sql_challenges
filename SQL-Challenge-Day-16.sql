f8ccce05f772b096509894449e6cdff1b98cc8b6
Gaurab Shrestha <gaurabian111@gmail.com>
2025-05-28 09:46:09 +0100

    File added

---
+++ SQL-Challenge-Day-16.sql
@@ -0,0 +1,10 @@
+-- JP Morgan, Chase, Bloomberg (Medium Level) hashtag #SQL Interview Question
+
+-- Bank of Ireland has requested that you detect invalid transactions in December 2022. 
+-- An invalid transaction is one that occurs outside of the bank's normal business hours. 
+-- The following are the hours of operation for all branches:
+
+-- Monday - Friday 09:00 - 16:00
+-- Saturday & Sunday Closed
+-- Irish Public Holidays 25th and 26th December
+-- Determine the transaction ids of all invalid transactions.
